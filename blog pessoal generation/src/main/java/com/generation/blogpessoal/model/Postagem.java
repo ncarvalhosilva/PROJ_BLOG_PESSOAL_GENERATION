0a5bd3f88bc18a767a60c74155ca09bb5e0731ca
Nayara <n.carvalhosilvabr@gmail.com>
2022-10-13 15:18:02 -0300

    inclusão da Camada Security

--- blog pessoal generation/src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ blog pessoal generation/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -37,6 +37,10 @@ public class Postagem {
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
 
 	
 	//GERAR GETTERS AND SETTERS
@@ -78,6 +82,14 @@ public class Postagem {
 
 	public void setTema(Tema tema) {
 		this.tema = tema;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
 	
 	
